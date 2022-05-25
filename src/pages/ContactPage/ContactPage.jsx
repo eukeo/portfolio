7ca82f9fab16c519b9c5dc162953a45f0be08a56
eukeo <sixsblooms@gmail.com>
2022-05-24 17:29:41 -0700

    remove number

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -9,7 +9,6 @@ export default function ContactPage() {
                     <img src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png" id="avatar"></img>
                     <h2>Gabriel Gomez</h2>
                     <h4 id="info">Contact Information</h4>
-                    <h4>Phone: (562) 454-9850</h4>
                     <h4>Email: gabegomez419@gmail.com</h4>
                 </div>
             </div>
