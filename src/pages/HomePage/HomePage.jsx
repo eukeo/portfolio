51e089050b717b9b09af268e88e7f2059b40fb98
eukeo <sixsblooms@gmail.com>
2023-01-20 17:20:21 -0800

    test push

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,29 +1,42 @@
-import AnimatedPage from '../../components/NavBar/AnimatedPage/AnimatedPage' 
-import './HomePage.css'
+import AnimatedPage from "../../components/NavBar/AnimatedPage/AnimatedPage";
+import "./HomePage.css";
 // import { Aos } from 'aos'
 // import "aos/dist/aos.css"
 // import { useEffect } from 'react'
 
 export default function HomePage() {
-
-
-    return (
-        <AnimatedPage>
-            <div id="container">
-                <div id="contents">
-                    <h1 id="name">Gabriel Gomez</h1>
-                    <h3 id="title">Software Engineer</h3>
-                    <h3 id="about-header">About Me</h3>
-                    <p id="about">Welcome! My name is Gabriel Gomez and I am a 28 year old Software Engineer born and raised in Los Angeles, California. As someone who has always enjoyed music, art, video games, and the slow development of the NFT space, I found myself stumbling across the beautiful world of code. I wrote my first few lines of CSS & HTML around 2005 on Myspace, but it wasn't until February 2022 when I had realized just how much of a passion I truly had for Front-End Development. After years of career exploration, I finally decided to attend General Assembly. For 3 months it was nothing but pursiung tons of new, difficult, but highly rewarding challenges. It has become extremely clear just how much love I have for learning more about the beautiful world of Front-End Development, as well as the dedication to create a great user experience.</p>
-                    {/* <a href="" id="contact">Contact</a> */}
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"></img>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"></img>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>       
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"></img>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"></img>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"></img>
-                </div>
-            </div>
-        </AnimatedPage>
-    )
+  return (
+    <AnimatedPage>
+      <div id="container">
+        <button></button>
+        <div id="contents">
+          <h1 id="name">Gabriel Gomez</h1>
+          <h3 id="title">Software Engineer</h3>
+          <h3 id="about-header">About Me</h3>
+          <p id="about">
+            Welcome! My name is Gabriel Gomez and I am a 28 year old Software
+            Engineer born and raised in Los Angeles, California. As someone who
+            has always enjoyed music, art, video games, and the slow development
+            of the NFT space, I found myself stumbling across the beautiful
+            world of code. I wrote my first few lines of CSS & HTML around 2005
+            on Myspace, but it wasn't until February 2022 when I had realized
+            just how much of a passion I truly had for Front-End Development.
+            After years of career exploration, I finally decided to attend
+            General Assembly. For 3 months it was nothing but pursiung tons of
+            new, difficult, but highly rewarding challenges. It has become
+            extremely clear just how much love I have for learning more about
+            the beautiful world of Front-End Development, as well as the
+            dedication to create a great user experience.
+          </p>
+          {/* <a href="" id="contact">Contact</a> */}
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"></img>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"></img>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"></img>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"></img>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"></img>
+        </div>
+      </div>
+    </AnimatedPage>
+  );
 }
